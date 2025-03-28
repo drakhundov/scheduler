b7418c1f66dfdcb99722ff858bd51c3e19bec69a
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-03-28 23:01:41 +0300

    Improved logging. Added file modes and made sure the start and end are logged.

--- include/logs.h
+++ include/logs.h
@@ -1,11 +1,15 @@
 #ifndef _LOGS_H
 #define _LOGS_H
 
+#include <fcntl.h>
+
+// Default configuration.
 #define LOGFILENAME "Scheduler.log"
+#define LOGMODE O_WRONLY | O_APPEND | O_CREAT
 
 typedef enum LOGTYPES { ERROR_LOG, INFO_LOG } Logtype;
 
-void init_logs(const char* filename);
+void init_logs(const char* filename, int mode);
 void end_logs();
 void logMsg(Logtype log_t, const char* fmt, ...);
 
--- src/logs.c
+++ src/logs.c
@@ -1,53 +1,63 @@
 #include "logs.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
+#include <unistd.h>
 
-int logfd;
+// Owner: rw
+// Group & others: r
+#define LOGPERMS 0644
 
-void init_logs(const char *filename) {
-	logfd = open(filename, O_WRONLY | O_CREAT, 0644);
-	if (logfd == -1) {
-		perror("Failed to open log file.");
-		exit(1);
-	}
+// Supposed to be inaccessible
+// anywhere else.
+static int logfd;
+
+void init_logs(const char *filename, int mode) {
+  logfd = open(filename, mode, LOGPERMS);
+  if (logfd == -1) {
+    perror("Failed to open the log file.");
+    exit(1);
+  }
+  logMsg(INFO_LOG, "Started logging");
 }
 
 // ! This is a helper function.
 // ! It is not supposed to be used
 // ! outside of the logging implementation.
 char *_get_timestamp() {
-	time_t now;
-	time(&now);
-	struct tm *local = localtime(&now);
-	char *timestamp = (char*) malloc(20);
-	if (timestamp == NULL) {
-		perror("Failed to allocate memory for timestamp.");
-		exit(1);
-	}
-	sprintf(timestamp, "%02d-%02d-%04d %02d:%02d:%02d", local->tm_mday, local->tm_mon + 1, local->tm_year + 1900,
-		local->tm_hour, local->tm_min, local->tm_sec);
-	return timestamp;
+  time_t now;
+  time(&now);
+  struct tm *local = localtime(&now);
+  char *timestamp = (char *)malloc(20);
+  if (timestamp == NULL) {
+    logMsg(ERROR_LOG, "Failed to allocate memory for timestamp");
+    perror("Failed to allocate memory for timestamp.");
+    exit(1);
+  }
+  sprintf(timestamp, "%02d-%02d-%04d %02d:%02d:%02d", local->tm_mday,
+          local->tm_mon + 1, local->tm_year + 1900, local->tm_hour,
+          local->tm_min, local->tm_sec);
+  return timestamp;
 }
 
 void end_logs() {
-	close(logfd);
+  logMsg(INFO_LOG, "Finished logging");
+  close(logfd);
 }
 
-void logMsg(Logtype log_t, const char* fmt, ...) {
-	va_list args;
-	va_start(args, fmt);
-	char tmp[1024];
-	char *type_msg = (log_t == ERROR_LOG) ? "ERROR" : "INFO";
-	vsnprintf(tmp, sizeof(tmp), fmt, args);
-	va_end(args);
-	char msg[1024];
-	sprintf(msg, "[%s] [%s] %s\n", _get_timestamp(), type_msg, tmp);
-	write(logfd, msg, strlen(msg));
-	fsync(logfd);  // Ensure the log message is written to disk.
+void logMsg(Logtype log_t, const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  char tmp[1024];
+  char *type_msg = (log_t == ERROR_LOG) ? "ERROR" : "INFO";
+  vsnprintf(tmp, sizeof(tmp), fmt, args);
+  va_end(args);
+  char msg[1024];
+  sprintf(msg, "[%s] [%s] %s\n", _get_timestamp(), type_msg, tmp);
+  write(logfd, msg, strlen(msg));
+  fsync(logfd);  // Ensure the log message is written to disk.
 }
