319519e43d4892dd019d3473f162b62525841bbb
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-03-29 16:10:34 +0300

    Debugged process module. process_constr: proper allocation and copying of prc_id process_destr: removed free(prc) (since it's store statically) run_process: resolved some issues

--- include/process.h
+++ include/process.h
@@ -5,7 +5,7 @@
 #include <sys/types.h>  // pid_t
 
 typedef struct {
-  char prc_id[20];
+  char *prc_id;
   int arr,     // Arrival
       pri,     // Priority
       exec_t,  // Execution time
@@ -13,8 +13,8 @@ typedef struct {
   pid_t pid;
 } Process;
 
-Process *process_constr(char *prc_id, int arr, int pri, int exec_t);
-void process_destr(Process *_prc);
+Process process_constr(char *prc_id, int arr, int pri, int exec_t);
+void process_destr(Process _prc);
 int process_comp(const void *a, const void *b);  // ! To be used by qsort.
 //* fin_prc tells if the Process is finished,
 //* so that it can be removed from the list.
--- src/process.c
+++ src/process.c
@@ -8,27 +8,27 @@
 
 #include "logs.h"
 
-Process *process_constr(char *prc_id, int arr, int pri, int exec_t) {
-  Process *new_prc = malloc(sizeof(Process));
-  if (new_prc == NULL) {
-    logMsg(ERROR_LOG, "Failed to allocate memory for new Process");
-    perror("Failed to allocate memory for new Process.");
+char *gExecPath;
+
+Process process_constr(char *prc_id, int arr, int pri, int exec_t) {
+  Process new_prc;
+  new_prc.prc_id = (char *)malloc(strlen(prc_id) + 1);
+  if (new_prc.prc_id == NULL) {
+    logMsg(ERROR_LOG, "Failed to allocate space for process id");
+    perror("Failed to allocate space for process id.");
     exit(1);
   }
-  strcpy(new_prc->prc_id, prc_id);
-  new_prc->arr = arr;
-  new_prc->pri = pri;
-  new_prc->exec_t = new_prc->rem_t = exec_t;
-  new_prc->pid = -1;
+  strcpy(new_prc.prc_id, prc_id);
+  new_prc.arr = arr;
+  new_prc.pri = pri;
+  new_prc.exec_t = new_prc.rem_t = exec_t;
+  new_prc.pid = -1;
   return new_prc;
 }
 
 // ! Must be called if Process is not in use
 // ! anymore to avoid memory leack.
-void process_destr(Process *_prc) {
-  free(_prc->prc_id);
-  free(_prc);
-}
+void process_destr(Process _prc) { free(_prc.prc_id); }
 
 int process_comp(const void *a, const void *b) {
   Process *j1 = (Process *)a;
@@ -49,37 +49,39 @@ int process_comp(const void *a, const void *b) {
   return 0;
 }
 
-int run_process(Process *process, bool *fin_prc) {
+int run_process(Process *prc, bool *fin_prc) {
   extern int gTimeQ;
   //* fin_prc tells if the Process is finished,
   //* so that it can be removed from the list.
   *fin_prc = false;
-  int alloc_t = (process->rem_t <= gTimeQ) ? process->rem_t : gTimeQ;
-  if (process->pid == -1) {
-    // The process hasn't been initialized yet.
-    logMsg(INFO_LOG, "Forking new process for %s", process->prc_id);
+  int alloc_t = (prc->rem_t <= gTimeQ) ? prc->rem_t : gTimeQ;
+  if (prc->pid == -1) {
+    // If the process is not started yet, fork and execute it.
+    logMsg(INFO_LOG, "Forking new process for %s", prc->prc_id);
     pid_t pid = fork();
     if (pid == 0) {
       // Executing the process.
-      logMsg(INFO_LOG, "Executing %s (PID: %d) using exec", process->prc_id,
+      logMsg(INFO_LOG, "Executing %s (PID: %d) using exec", prc->prc_id,
              getpid());
       char buffer[20];
-      sprintf(buffer, "%d", alloc_t);
-      execlp("./ProcessExec", "./ProcessExec", process->prc_id, buffer, NULL);
+      snprintf(buffer, sizeof(buffer), "%d", alloc_t);
+      execlp(gExecPath, gExecPath, prc->prc_id, buffer, NULL);
+      perror("Failed to execute the process.");
+      exit(1);
     } else if (pid > 0) {
-      process->pid = pid;
+      prc->pid = pid;
       sleep(alloc_t);
-      process->rem_t -= alloc_t;
-      if (process->rem_t > 0) {
+      prc->rem_t -= alloc_t;
+      if (prc->rem_t > 0) {
         logMsg(INFO_LOG,
                "%s ran for %d seconds. Time slice expired - Sending SIGSTOP",
-               process->prc_id, alloc_t);
-        kill(process->pid, SIGSTOP);
+               prc->prc_id, alloc_t);
+        kill(prc->pid, SIGSTOP);
       } else {
         logMsg(INFO_LOG, "%s completed execution. Terminating (PID: %d)",
-               process->prc_id, process->pid);
+               prc->prc_id, prc->pid);
         // ! Ensure the process safely ends.
-        waitpid(process->pid, NULL, 0);
+        waitpid(prc->pid, NULL, 0);
         *fin_prc = true;  // To be removed from the process list.
       }
     } else {
@@ -89,92 +91,21 @@ int run_process(Process *process, bool *fin_prc) {
     }
   } else {
     // Resume existing process.
-    kill(process->pid, SIGCONT);
+    kill(prc->pid, SIGCONT);
     sleep(alloc_t);
-    process->rem_t -= alloc_t;
-    if (process->rem_t > 0) {
+    prc->rem_t -= alloc_t;
+    if (prc->rem_t > 0) {
       logMsg(INFO_LOG,
              "%s ran for %d seconds. Time slice expired - Sending SIGSTOP",
-             process->prc_id, alloc_t);
-      kill(process->pid, SIGSTOP);
+             prc->prc_id, alloc_t);
+      kill(prc->pid, SIGSTOP);
     } else {
       logMsg(INFO_LOG, "%s completed execution. Terminating (PID: %d)",
-             process->prc_id, process->pid);
-      waitpid(process->pid, NULL, 0);
+             prc->prc_id, prc->pid);
+      waitpid(prc->pid, NULL, WNOHANG);
       *fin_prc = true;
     }
-    logMsg(INFO_LOG, "%s remaining time: %d\n", process->prc_id, process->rem_t);
+    logMsg(INFO_LOG, "%s remaining time: %d", prc->prc_id, prc->rem_t);
   }
   return alloc_t;
 }
-
-// int run_Process(Process *Process, bool *fin_prc) {
-//   extern int gTimeQ;
-//   *fin_prc =
-//       false;    // A flag that specifies whether the process should be
-//       finished.
-//   int alloc_t;  // Allocated time.
-//   int t = Process->rem_t;
-//   if (t <= gTimeQ) {
-//     alloc_t = t;
-//   } else {
-//     alloc_t = gTimeQ;
-//   }
-//   if (Process->pid == -1) {
-//     pid_t pid = fork();
-//     if (pid == -1) {
-//       logMsg(ERROR_LOG,
-//              "Fork failed: Resource temporarily unavailable for Process %s",
-//              Process->prc_id);
-//       perror("Fork failed");
-//       exit(1);
-//     }
-//     logMsg(INFO_LOG, "Forking new process for %s", Process->prc_id);
-//     if (pid == 0) {
-//       // Child process: Executing the Process.
-//       logMsg(INFO_LOG, "Executing %s (PID: %d) using exec", Process->prc_id,
-//              getpid());
-//       char buffer[20];
-//       sprintf(buffer, "%d", t);
-//       execlp("./Process_exec", "./Process_exec", buffer, NULL);
-//       // If execlp fails
-//       logMsg(ERROR_LOG, "execlp failed for Process %s", Process->prc_id);
-//       perror("execlp failed");
-//       exit(1);
-//     } else if (pid > 0) {
-//       // Parent process: Managing the Process.
-//       Process->pid = pid;
-//       sleep(alloc_t);
-//       Process->rem_t -= alloc_t;
-//       if (Process->rem_t > 0) {
-//         logMsg(INFO_LOG,
-//                "%s ran for %d seconds. Time slice expired - Sending SIGSTOP",
-//                Process->prc_id, alloc_t);
-//         kill(Process->pid, SIGSTOP);
-//       } else {
-//         logMsg(INFO_LOG, "%s completed execution. Terminating (PID: %d)",
-//                Process->prc_id, Process->pid);
-//         waitpid(Process->pid, NULL, 0);
-//         *fin_prc = true;
-//       }
-//     }
-//   } else {
-//     // Resume the existing Process.
-//     kill(Process->pid, SIGCONT);
-//     sleep(alloc_t);
-//     Process->rem_t -= alloc_t;
-//     if (Process->rem_t > 0) {
-//       logMsg(INFO_LOG,
-//              "%s ran for %d seconds. Time slice expired - Sending SIGSTOP",
-//              Process->prc_id, alloc_t);
-//       kill(Process->pid, SIGSTOP);
-//     } else {
-//       logMsg(INFO_LOG, "%s completed execution. Terminating (PID: %d)",
-//              Process->prc_id, Process->pid);
-//       waitpid(Process->pid, NULL, 0);
-//       *fin_prc = true;
-//     }
-//   }
-//   logMsg(INFO_LOG, "%s remaining time: %d\n", Process->prc_id, Process->rem_t);
-//   return alloc_t;
-// }
